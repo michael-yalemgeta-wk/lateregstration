7d6a5d349b28bac9bd7636880dc669221ef0fedc
Michael yalemgeta <mickeywk@outlook.com>
2026-05-05 03:25:58 +0300

    Remove 'Other' option from gender select
    Removed 'Other' option from gender selection.

--- app/page.tsx
+++ app/page.tsx
@@ -293,7 +293,6 @@ export default function Home() {
                     <option value="" disabled>{t.select}</option>
                     <option value="Male">{t.male}</option>
                     <option value="Female">{t.female}</option>
-                    <option value="Other">{t.other}</option>
                   </select>
                   <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 text-slate-500">
                     <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
